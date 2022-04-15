95e5386d56339e139f7ebc5b07f548ba573cfc89
Max_Kutuzov <sumdy@mail.ru>
2022-04-15 16:43:34 +0300

    Fix Name Style

---
+++ src/components/PostList/PostList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PostListItem from '../PostListItem/PostListItem';
+import { ListGroup } from 'reactstrap';
+import './PostList.css';
+
+const PostList = ({ posts, onDelete, onToggleImp, onToggleLike }) => {
+    const elem = posts.map((item) => {
+        const { id, ...itemProps } = item;
+        return (
+            <li key={item.id} className="list-group-item">
+                <PostListItem
+                    {...itemProps}
+                    onDelete={() => {
+                        onDelete(id);
+                    }}
+                    onToggleImp={() => {
+                        onToggleImp(id);
+                    }}
+                    onToggleLike={() => {
+                        onToggleLike(id);
+                    }}
+                />
+            </li>
+        );
+    });
+    return <ListGroup className="app-list">{elem}</ListGroup>;
+};
+
+export default PostList;
---
+++ src/components/SearchPanel/SearchPanel.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './SearchPanel.css';
+
+const SearchPanel = (props) => {
+    const onUpSearch = (e) => {
+        let trm = e.target.value;
+
+        props.onUpdateSearch(trm);
+    };
+
+    return (
+        <input
+            className="form-control search-input"
+            type="text"
+            placeholder="Поиск по записям"
+            onChange={onUpSearch}
+        />
+    );
+};
+export default SearchPanel;
